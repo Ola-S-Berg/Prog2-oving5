57f4ed98b7113bf5852b5194ec5a5c89722d1b78
Ola Syrstad Berg <olasyrstadberg@yahoo.com>
2025-03-10 23:59:46 +0100

    Updated GUI.java
    Created a basic GUI

--- src/main/java/edu/ntnu/idatt2003/cardgame/GUI.java
+++ src/main/java/edu/ntnu/idatt2003/cardgame/GUI.java
@@ -10,7 +10,25 @@ import javafx.stage.Stage;
 public class GUI extends Application {
 
   private DeckOfCards deck = new DeckOfCards();
+  private Label handDisplay;
+  private Label resultDisplay;
 
   @Override
-  public void start(Stage primaryStage) {}
+  public void start(Stage primaryStage) {
+    Button dealButton = new Button("Deal Hand");
+    Button playButton = new Button("Check Hand");
+    handDisplay = new Label("Hand: ");
+    resultDisplay = new Label("Result: ");
+
+    VBox layout = new VBox(10, handDisplay, resultDisplay, dealButton, resultDisplay);
+
+    Scene scene = new Scene(layout, 300, 200);
+    primaryStage.setScene(scene);
+    primaryStage.setTitle("Card Game");
+    primaryStage.show();
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
