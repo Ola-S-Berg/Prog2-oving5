7c9988059a9909474ddba171017f59dedc31be22
Ola Syrstad Berg <olasyrstadberg@yahoo.com>
2025-03-10 23:53:22 +0100

    Created GUI.java

---
+++ src/main/java/edu/ntnu/idatt2003/cardgame/GUI.java
@@ -0,0 +1,16 @@
+package edu.ntnu.idatt2003.cardgame;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+
+  private DeckOfCards deck = new DeckOfCards();
+
+  @Override
+  public void start(Stage primaryStage) {}
+}
