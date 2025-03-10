dcb745b08703259ceaaa241d68b1276912cb6cb6
Ola Syrstad Berg <olasyrstadberg@yahoo.com>
2025-03-10 23:31:50 +0100

    Initial commit

---
+++ src/test/java/edu/ntnu/idatt2003/cardgame/PlayingCardTest.java
@@ -0,0 +1,28 @@
+package edu.ntnu.idatt2003.cardgame;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PlayingCardTest {
+
+  @Test
+  void getAsString() {
+  }
+
+  @Test
+  void getSuit() {
+  }
+
+  @Test
+  void getFace() {
+  }
+
+  @Test
+  void testEquals() {
+  }
+
+  @Test
+  void testHashCode() {
+  }
+}
