450346c505655b047ec6ffa057bf093a4b45b527
Ola Syrstad Berg <olasyrstadberg@yahoo.com>
2025-03-10 23:41:40 +0100

    Created deck of cards constructor

--- src/main/java/edu/ntnu/idatt2003/cardgame/DeckOfCards.java
+++ src/main/java/edu/ntnu/idatt2003/cardgame/DeckOfCards.java
@@ -1,5 +1,25 @@
 package edu.ntnu.idatt2003.cardgame;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 public class DeckOfCards {
 
+  private final List<PlayingCard> deck;
+
+  private final char [] suit = { 'S', 'H', 'D', 'C' };
+
+  /**
+   * Creates a deck of 52 cards. s = suit, f = faces.
+   */
+  public DeckOfCards() {
+    deck = new ArrayList<>();
+
+    for (char s : suit) {
+      for (int f = 1; f <= 13; f++) {
+        deck.add(new PlayingCard(s, f));
+      }
+    }
+  }
 }
